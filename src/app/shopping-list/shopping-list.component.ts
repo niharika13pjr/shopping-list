b248ac62c1213de43fd7aa1bf1828a7f1080e9a8
Niharika Pujari <niharika13pjr@gmail.com>
2018-07-06 15:38:04 -0400

    Working recipe and add to shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -10,6 +10,10 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
   ];
+
+  onIngredientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+  }
   constructor() { }
 
   ngOnInit() {
