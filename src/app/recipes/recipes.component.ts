bb8fe3b7083cd4fb62da05ec2afbc9d88b9dfdf5
Niharika Pujari <niharika13pjr@gmail.com>
2018-07-07 15:56:59 -0400

    Added Shopping Service

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -15,7 +15,8 @@ export class RecipesComponent implements OnInit {
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
+    //subscribe() to listen to event
+    this.recipeService.recipeSelected.subscribe( 
       (recipe: Recipe) => {
         this.selectedRecipe = recipe;
       }
